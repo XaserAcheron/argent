7e0b62a80e130fce3277e31cf5f27f9568c2ca29
Xaser Acheron <xaser.88@gmail.com>
2016-07-14 14:20:31 -0500

    updated grunt build tasks to compile scripts with GDCC

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,34 +7,53 @@ module.exports = function (grunt) {
 	
 	var config = {
 		name: 'xa-argen',
-		src: 'src',
+		dir: {
+			src: 'src',
+			dest: 'dist',
+			gdcc: 'gdcc',
+		},
+		scripts: [
+			'argent.acs'
+		],
 		pkg: require('./package.json')
 	};
 	
 	grunt.initConfig({
 		conf: config,
 
+		shell: {
+			compile: {
+				command: config.scripts.map(function(script) {
+					return [
+						'"./<%= conf.dir.gdcc %>/gdcc-acc"',
+						'--lib-path <%= conf.dir.gdcc %>/lib',
+						'./<%= conf.dir.src %>/scripts/' + script,
+						'--output ./<%= conf.dir.src %>/acs/' + script.replace('.acs', '.o')
+					].join(' ');
+				}).join('&&')
+			}
+		},
 		compress: {
 			main: {
 				options: {
 					mode: 'zip',
-					archive: './dist/<%= conf.name %>.pk3'
+					archive: './<%= conf.dir.dest %>/<%= conf.name %>.pk3'
 				},
 				files: [
-					{expand: true, cwd: 'src/', src: ['**'], dest: '/'}
+					{expand: true, cwd: '<%= conf.dir.src %>/', src: ['**'], dest: '/'}
 				]
 			}
 		},
 		copy: {
 			main: {
-				src: 'src/wadinfo.txt',
-				dest: './dist/<%= conf.name %>.txt'
+				src: '<%= conf.dir.src %>/wadinfo.txt',
+				dest: './<%= conf.dir.dest %>/<%= conf.name %>.txt'
 			},
 			dist: {
 				expand: true,
 				dot: true,
-				cwd: 'dist',
-				dest: 'dist/',
+				cwd: '<%= conf.dir.dest %>',
+				dest: '<%= conf.dir.dest %>/',
 				src: '<%= conf.name %>.*',
 				rename: function(dest, src) {
 					return dest + src.replace(/(\.[\w\d_-]+)$/i, '_<%= conf.pkg.version %>$1');
@@ -44,7 +63,7 @@ module.exports = function (grunt) {
 	});
 	
 	// task aliases
-	grunt.registerTask('build', ['compress', 'copy:main']);
-	grunt.registerTask('dist', ['compress', 'copy:main', 'copy:dist']);
+	grunt.registerTask('build', ['shell:compile', 'compress', 'copy:main']);
+	grunt.registerTask('dist', ['shell:compile', 'compress', 'copy:main', 'copy:dist']);
 	grunt.registerTask('default', ['build']);
 };
