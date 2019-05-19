688a0fc270d144884132f26521a7248c913827e2
Xaser Acheron <xaser.88@gmail.com>
2019-05-19 01:54:55 -0500

    added wadinfo.txt transformation to build.py

--- build.py
+++ build.py
@@ -2,28 +2,33 @@
 import os
 import subprocess
 import zipfile
+import datetime
 
 from glob import iglob
 from shutil import copyfile
 from argparse import ArgumentParser
 from configparser import ConfigParser
 
+today = datetime.datetime.now().strftime('%m/%d/%Y')
+
 #
 # Build main package (as .pk3, a good ol' zip, really)
 #
 def makepkg(sourcePath, destPath):
 	destination = destPath + ".pk3"
+	wadinfoPath = destPath + ".txt" # just assume this, 'cause we can.
 
 	print ("\n-- Compressing {filename} --".format (filename=destination))
 	filelist = []
 	for path, dirs, files in os.walk (sourcePath):
 		for file in files:
-			# Remove sourcepath from filenames in zip
-			splitpath = path.split(os.sep)[1:]
-			splitpath.append(file)
-			name = os.path.join(*splitpath)
+			if file != "wadinfo.txt": # special exception
+				# Remove sourcepath from filenames in zip
+				splitpath = path.split(os.sep)[1:]
+				splitpath.append(file)
+				name = os.path.join(*splitpath)
 
-			filelist.append((os.path.join (path, file), name,))
+				filelist.append((os.path.join (path, file), name,))
 
 	distzip = zipfile.ZipFile(destination, "w", zipfile.ZIP_DEFLATED)
 	current = 1
@@ -32,20 +37,26 @@ def makepkg(sourcePath, destPath):
 		distzip.write(*file)
 		current += 1
 
+	# for wadinfo.txt, use the transformed file in the output dir
+	# rather than the template one with x.x.x's still in it
+	distzip.write(wadinfoPath, 'wadinfo.txt')
+
 #
 # Copy wadinfo TXT file to output directory.
 #
-def maketxt(sourcePath, destPath):
+def maketxt(sourcePath, destPath, version):
 	textname = os.path.join (sourcePath, "wadinfo.txt")
 	destname = destPath + ".txt"
 
 	print("\n-- Copying {source} to {dest} --".format (source=textname, dest=destname))
 
-	sourcefile = open (textname, "rb")
-	textfile = open (destname, "wb")
+	sourcefile = open (textname, "rt")
+	textfile = open (destname, "wt")
 
-	content = sourcefile.read()
-	textfile.write(sourcefile.read())
+	for line in sourcefile:
+		line = line.replace('x.x.x', version)
+		line = line.replace('xx/xx/xxxx', today)
+		textfile.write(line)
 
 	textfile.close()
 	sourcefile.close()
@@ -88,7 +99,7 @@ if __name__ == "__main__":
 
 		destPath = os.path.join(distDir, fileName)
 
-		maketxt(sourceDir, destPath)
+		maketxt(sourceDir, destPath, version)
 		makepkg(sourceDir, destPath)
 
 		if(args.dist):
